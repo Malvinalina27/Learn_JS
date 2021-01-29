e163ffcc4df5b1d5fceda81bd921d08bffcc67b6
Malvinalina27 <rasberry-07@mail.ru>
2021-01-29 22:35:49 +0300

    Присвоила значение переменным, вывела в консоль данные

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let money = 50000;
+let income = 'фриланс'; 
+let addExpenses = 'ЖКХ, животные, книги'; 
+let deposit = true; 
+let mission = 200000;
+let period = 12;
+let budgetDay = money / 30;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей');
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(', '));
+console.log(budgetDay);
