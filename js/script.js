2a5e288fffaba87a3d531d9957b2b81298b29168
Malvinalina27 <rasberry-07@mail.ru>
2021-02-12 13:22:42 +0300

    исправила вывод в консоли calcSavedMoney

--- js/script.js
+++ js/script.js
@@ -126,4 +126,4 @@ for (let key in appData) {
   console.log('Наша программа включает в себя данные: ' + key + ' ' + appData[key]);
 }
 
-console.log(appData.percentDeposit, appData.moneyDeposit, appData.calcSavedMoney); 
+console.log(appData.percentDeposit, appData.moneyDeposit, appData.calcSavedMoney()); 
