e0317f120ad90a08f08fda436d6941ad3cf1b851
Malvinalina27 <rasberry-07@mail.ru>
2021-02-26 23:09:41 +0300

    исправила вывод времени на более короткий

--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
 window.addEventListener('DOMContentLoaded', () => {
 
-  //const text = document.querySelector('.text');
   const hi = document.querySelector('.hi');
   const today = document.querySelector('.today');
   const now = document.querySelector('.now');
   const newYear = document.querySelector('.new-year');
-
   const dateNow = new Date();
+
 
   function countTimer() {
     // 1 строка
@@ -39,40 +38,22 @@ window.addEventListener('DOMContentLoaded', () => {
     today.innerHTML = `Сегодня: ${timeMas[dateNow.getDay()]}`;
 
     //3 строка
-    function getNow() {
-      const hours = dateNow.getHours();
-      const minutes = dateNow.getMinutes();
-      const seconds = dateNow.getSeconds();
-
-      const ampm = hours < 12 ? ' AM' : ' PM';
-
-      const timeNow = [ hours - 12,
-        (minutes < 10 ? "0" + minutes : minutes),
-        (seconds < 10 ? "0" + seconds : seconds)]
-        .join(':') + ampm;
-      now.innerHTML = 'Текущее время: ' + timeNow;
-
-      setTimeout(getNow, 1000);
-    }
-    getNow();
-    //setInterval(getNow, 1000);
-
+    const getNowTime = dateNow.toLocaleTimeString('en');
+    now.innerHTML = 'Текущее время: ' + getNowTime;
 
     // 4 строка
-    function countNY() {
-      const now = new Date().getDate(),
-        dateStop = new Date('01 January 2022 00:00').getTime(),
-        timeRemaining = (dateStop - now) / 1000,
-        day = Math.floor(timeRemaining / 60 / 60 / 24);
-      newYear.innerHTML = `До нового года осталось ${day} дней`;
+    const  dateStop = new Date('01 January 2022 00:00').getTime(),
+      timeRemaining = (dateStop - dateNow.getTime()) / 1000,
+      day = Math.floor(timeRemaining / 60 / 60 / 24);
 
-      if (timeRemaining < 0) {
-        newYear.innerHTML = 'С Новым годом!';
-      }
+    if (timeRemaining < 0) {
+      newYear.innerHTML = 'С Новым годом!';
+    } else {
+      newYear.innerHTML = `До нового года осталось ${day} дней`;
     }
-    countNY();
+
   }
-  countTimer();
+
   setInterval(countTimer, 1000);
 
 });
