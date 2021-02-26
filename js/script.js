68cbfd16460a15445041dbe966de47353de7edd1
Malvinalina27 <rasberry-07@mail.ru>
2021-02-26 14:54:12 +0300

    добавила таймер формата 00:00:00, реализованный через setInterval

--- js/script.js
+++ js/script.js
@@ -1,1 +1,47 @@
-'use strict';
+window.addEventListener('DOMContentLoaded', () => {
+
+
+  //Timer
+  function countTimer(deadline) {
+    const timerHours = document.querySelector('#timer-hours'),
+      timerMinutes = document.querySelector('#timer-minutes'),
+      timerSeconds = document.querySelector('#timer-seconds');
+
+    function getTimeRemaining() {
+      const dateStop = new Date(deadline).getTime(),
+        dateNow = new Date().getTime(),
+        timeRemaining = (dateStop - dateNow) / 1000,
+        seconds = Math.floor(timeRemaining % 60),
+        minutes = Math.floor((timeRemaining / 60) % 60),
+        hours = Math.floor(timeRemaining / 60 / 60);
+      return { dateStop, timeRemaining, hours, minutes, seconds };
+    }
+
+    function updateClock() {
+      const timer = getTimeRemaining();
+
+      if (timer.timeRemaining < 0) {
+        timerHours.textContent = '00';
+        timerMinutes.textContent = '00';
+        timerSeconds.textContent = '00';
+        clearInterval(updateClock);
+      } else {
+        timerHours.textContent = timer.hours;
+        if (timer.hours < 10) timerHours.textContent = '0' + timer.hours;
+        timerMinutes.textContent = timer.minutes;
+        if (timer.minutes < 10) timerMinutes.textContent = '0' + timer.minutes;
+        timerSeconds.textContent = timer.seconds;
+        if (timer.seconds < 10) timerSeconds.textContent = '0' + timer.seconds;
+      }
+    }
+
+    updateClock();
+
+    setInterval(updateClock, 1000);
+
+
+  }
+
+  countTimer('27 February 2021 15:00');
+
+});
