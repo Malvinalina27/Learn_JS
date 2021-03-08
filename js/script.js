a12fe588c6eb83384c97780a61df8fedab5ea150
Malvinalina27 <rasberry-07@mail.ru>
2021-03-09 02:15:46 +0300

    добавила json и ajax

--- js/script.js
+++ js/script.js
@@ -410,4 +410,56 @@ window.addEventListener('DOMContentLoaded', () => {
   };
   form();
 
+  // send-aiax-form
+  const sendForm = () => {
+    const errorMessage = 'Что-то пошло не так...';
+    const loadMessage = 'Загрузка...';
+    const successMessage = 'Спасибо! Мы скоро с вами свяжемся!';
+
+    const form = document.querySelectorAll(['#form1', '#form2', '#form3']);
+
+    const statusMessage = document.createElement('div');
+    statusMessage.style.cssText = 'font-size: 2 rem;';
+
+    form.forEach(elem => {
+      elem.addEventListener('submit', event => {
+        event.preventDefault();
+        elem.appendChild(statusMessage);
+        statusMessage.textContent = loadMessage;
+        const formData = new FormData(elem);
+        let body = {};
+        formData.forEach((val, key) => {
+          body[key] = val;
+        });
+        postData(body, () => {
+          statusMessage.textContent = successMessage;
+        }, error => {
+          statusMessage.textContent = errorMessage;
+          console.error(error);
+        });
+      });
+    });
+
+
+    const postData = (body, outputData, errorData) => {
+      const request = new XMLHttpRequest();
+      request.addEventListener('readystatechange', () => {
+        if (request.readyState !== 4) {
+          return;
+        }
+
+        if (request.status === 200) {
+          outputData();
+        } else {
+          errorData(request.status);
+        }
+      });
+      request.open('POST', './server.php');
+      request.setRequestHeader('Content-Type', 'application/json');
+
+      request.send(JSON.stringify(body));
+    };
+  };
+  sendForm();
+
 });
