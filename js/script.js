be448dc71b96e976282340122b909716bf456b0a
Malvinalina27 <rasberry-07@mail.ru>
2021-03-09 22:46:29 +0300

    изменила функцию ourCommand

--- js/script.js
+++ js/script.js
@@ -355,22 +355,26 @@ window.addEventListener('DOMContentLoaded', () => {
   calc(100);
 
 
-  // command !
+  // command
   const ourCommand = () => {
-    //const command = document.querySelector('.command');
-    const commandPhoto = document.querySelectorAll('.command__photo');
-
-    commandPhoto.forEach((e, i) => {
-
-      commandPhoto[i].addEventListener('mouseover', event => {
-        event.target.src = event.target.dataset.img;
-      });
-
-    /*   commandPhoto[i].addEventListener('mouseout', event => {
-        console.log(event.target);
-      }); */
+    const command = document.querySelector('.command');
+    let support;
+    command.addEventListener('mouseover', event => {
+      const target = event.target;
+      if (target.matches('img')) {
+        support = target.src;
+        target.src = target.dataset.img;
+        target.dataset.img = support;
+      }
     });
-
+    command.addEventListener('mouseout', event => {
+      const target = event.target;
+      if (target.matches('img')) {
+        support = target.src;
+        target.src = target.dataset.img;
+        target.dataset.img = support;
+      }
+    });
   };
   ourCommand();
 
